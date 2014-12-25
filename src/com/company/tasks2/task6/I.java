c1277e99347e374aab6982c9e095dc8e2c4431ff
ahmoxa <ahtoh9106@mail.ru>
2014-12-25 23:48:55 +0400

    done tasks2 ( 7+-)

--- src/com/company/tasks2/task6/I.java
+++ src/com/company/tasks2/task6/I.java
@@ -8,16 +8,13 @@ public class I {
     public static void main(String[] args) {
         System.out.println("Input A type byte");
         Scanner scanner = new Scanner(System.in);
-        int a = 7;//scanner.nextInt();
+        int a = scanner.nextInt();
 
-        int[] n = new int[] {0, 0, 0, 0, 0, 0, 0, 0};
-        int m = 2;
-        for(int i = 0;; i++)
-        {
-            n[n.length - 1 - i] = a % 2;
-            if(a < 2) break;
-            a/=2;
-        }
+        for (int i = 7; i >= 0; i --){
+            int mask = (1<<i);
+            int result = (a & mask)>>i;
+            System.out.print(result);
+      }
 
 
     }
