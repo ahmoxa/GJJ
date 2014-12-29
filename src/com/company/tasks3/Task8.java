3536f49238270256ab1ad21711e4cbac4fcc64d3
Anton Vlasyuk <ahtoh9106@mail.ru>
2014-12-29 15:19:04 +0400

    Tasks 3 (Task8 done)

---
+++ src/com/company/tasks3/Task8.java
@@ -0,0 +1,12 @@
+package com.company.tasks3;
+
+
+public class Task8 {
+    public static void main(String[] args) {
+        int a = 55;
+        int b = 11;
+
+        int result = (a%b) * (b%a) + 1;
+        System.out.println(result);
+    }
+}
