08ba83bf7f5d0a9c8a67be7a4f4dd40feca79a18
Anton Vlasyuk <ahtoh9106@mail.ru>
2015-01-15 12:40:22 +0400

    Task7(12.24b complete)

---
+++ src/com/company/tasks7/Task_12_24B.java
@@ -0,0 +1,23 @@
+package com.company.tasks7;
+
+
+public class Task_12_24b{
+
+    public static void main(String[] args) {
+        int[][] arr = new int[7][7];
+        fillArr(arr);
+        Utils.printArr(arr);
+    }
+
+    public static void fillArr(int[][] arr){
+        for (int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr[i].length; j++){
+                if(i + j < arr.length){
+                    arr[i][j] = i + j + 1;
+                }else{
+                    arr[i][j] = 1 + i + j - arr.length;
+                }
+            }
+        }
+    }
+}
